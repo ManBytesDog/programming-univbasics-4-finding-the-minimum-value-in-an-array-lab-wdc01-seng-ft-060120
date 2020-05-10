ccfc84a3d13b4c9360cb440ec89edc7a620ce257
T.J. Marstiller <thomasj.marstiller@yahoo.com>
2020-05-10 15:42:44 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -15,7 +15,7 @@ def find_min_value(array)
   count = 0 
   min_value = nil
   while count < array.length do 
-    if (min_value > array[count]) || (min_value == nil)
+    if  (min_value > array[count]) || (min_value == nil)
       min_value = array[count]
     end 
   count += 1
