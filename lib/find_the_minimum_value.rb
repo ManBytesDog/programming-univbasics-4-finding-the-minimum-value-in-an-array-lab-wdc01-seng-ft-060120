b7e0a7af92974b1cbbc15c9b3eb052f79e9fb71a
T.J. Marstiller <thomasj.marstiller@yahoo.com>
2020-05-10 15:44:17 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,21 +1,21 @@
-#def find_min_value(array)
- # count = 0 
-  #min_value = 100000000
-  #while count < array.length do 
-   #if min_value > array[count]
-     # min_value = array[count]
-    #end 
-  #count += 1
-  #end
-  #min_value
-#end
+def find_min_value(array)
+  count = 0 
+  min_value = 100000000
+  while count < array.length do 
+   if min_value > array[count]
+     min_value = array[count]
+    end 
+  count += 1
+  end
+  min_value
+end
 
 
 def find_min_value(array)
   count = 0 
   min_value = nil
   while count < array.length do 
-    if(min_value == nil) ||  (min_value > array[count])
+    if(min_value == nil) || (min_value > array[count])
       min_value = array[count]
     end 
   count += 1
